6a266f8ece69b489eaffecda36bd534b297ca6b8
刘敏 <iisdean.qq.com>
2020-04-07 19:08:18 +0800

    12

---
+++ 202004/a20200403bbyr_v/bridgeTpl_1302.js
@@ -0,0 +1,45 @@
+/*
+                    iBtnGraySwitch:是否开启按钮置灰功能 修改CSS样式名称进行覆盖 btn-zhih 
+                    willQHoldFlow:是否开启用资格查询，用于显示剩余资格数 以上两者只要有一个开启，就查询资格流程
+                    iGameType:游戏类型 1 端游 2 手游
+                    iQualType:资格类型 到openid/qq取1  其他都算2
+                    qZigeFlow:查询资格的流程号
+                    qJfFlow:查询积分的流程号
+                    qDsFlow:查询条件数据的流程号
+                    iSighQueryFlow:查询签到信息的流程号
+                    iMyAddressId:收货地址填写的提交流程号
+                    iDecryptFlow:解密邀请码的提交流程号
+                    iInviteFlow:快速邀请的提交流程号
+                    needAddress=1:是否需要收货地址填写功能
+                    iActivityId:ams活动id
+                    iBindAreaId:大区提交绑定流程
+                    iInitAreaId:大区查询流程
+                    iLotteryId:抽奖模块id，若有多个取一个
+                    iMyListId:我的礼包查询流程号
+                    iGiftArea:我的礼包是否显示大区一列
+                    iMrmsBind:是否绑定礼包单
+                    sService:业务名称
+*/
+window.iWillMergeCode =1;
+window.iBtnGraySwitch =1;
+window.willQHoldFlow =0;
+window.iGameType =2;
+window.iQualType =2;
+window.qZigeFlow =655095;
+window.qJfFlow =0;
+window.qDsFlow =0;
+window.iSighQueryFlow=0;
+window.iMyAddressId=655086;
+window.iDecryptFlow=0;
+window.iInviteFlow=0;
+window.needAddress=0;
+window.iActivityId=296559;
+window.iBindAreaId = 655081;
+window.iInitAreaId = iBindAreaId + 1;
+window.iLotteryId = 340398;
+window.iMyListId = 655083;
+window.iGiftArea = 1;
+window.sService = 'cfm';
+window.gLubanId = 1846;
+window.iMrmsBind = 1;
+var gLogCfg = {"iLoginTypeSwitch":3,"iBindZoneSwitch":1,"iMyGiftBagSwitch":1,"iMyAddressSwitch":1,"iMyOrderlstSwitch":1,"iOpenlink":0,"iZoneShowType":0,"sZoneIdsToHide":""};
